89f10f648d4441b96b22fd1fe8c6b3e9be34546c
Alex Harter <93167959+AlexHarter@users.noreply.github.com>
2023-10-26 10:34:13 -0500

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,247 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+ 
+  ##Configure X11; Desktop Environment or Window Manager; and Display Manager.
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+
+    # Configure keymap in X11.
+    layout = "us";
+    xkbVariant = "";
+
+    # i3 Window Manager
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        st
+        (st.overrideAttrs (oldAttrs: rec {
+          # Using a local file
+          configFile = writeText "config.def.h" (builtins.readFile ./st-config.h);
+          
+          postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+        }))
+        dmenu
+        i3status
+        i3lock
+        #i3blocks #look into this later.
+        brightnessctl
+        pw-volume
+        nnn
+        pcmanfm
+      ];
+    };
+
+     libinput.touchpad.naturalScrolling = true;
+
+  };
+
+#      # GNOME
+#      displayManager = {
+#        gdm.enable = true;
+#      };
+#     
+#      desktopManager = {
+#        gnome.enable = true;
+#      extraPackages = with pkgs; [
+#        gnome.gnome-tweaks
+#        gnomeExtensions.tiling-assistant
+#      ];
+#    };
+#  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.alex = {
+    isNormalUser = true;
+    description = "Alex";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "openssl-1.1.1w"
+      "python-2.7.18.6"
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Basics
+    vim
+    pkgs.brave
+    wine
+    libreoffice
+    git
+    gimp
+    discord
+    qbittorrent
+    usbutils
+    pdftk
+    zoom-us #for PGP-DSBA
+
+    # Coding
+    sublime4
+    jupyter #PGP-DSBA uses this, but I can also use Google Colab.
+    #jetbrains.pycharm-community #Try out later.
+    #spyder #Try out later.
+  
+    ## Languages
+    python3Full
+    postgresql
+
+    # Publishing
+    scribus # Even if I learn LaTeX, I think there will be more graphics-heavy use cases where I can also incorporate LaTeX, Lilypond, etc.
+    ## Fonts
+    eb-garamond
+    monocraft
+
+    ## LaTeX
+    texlive.combined.scheme-full #Is this the one I need?  I figured this was the fullest option.
+
+    # Music
+    audacity
+
+    ## Notation
+    lilypond-with-fonts
+    frescobaldi    
+    musescore #I want to replace MuseScore with Lilypond because the type of music and projects I want to do are much easier with a text based editor.
+
+    ## DAWs
+    reaper
+    #renoise
+
+    ## trackers 
+    milkytracker
+    #sunvox
+
+    ## Plugins
+    #dexed
+    #distrho
+    #decent-smapler
+
+    # Gaming
+    #steam
+
+    ## Emulation
+    #dolphin-emu
+    #duckstation
+    #pcsx2
+    #mgba
+
+    ## Minecraft
+    #prismlauncher
+    #jdk
+
+    # Android
+    #android-tools
+    #heimdall-gui
+    
+    # Other
+    obsidian
+    chrysalis
+    #davinci-resolve
+  ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+#Glory be to God for All Things!
+}
